1a9f1394a9b766bd9b6d2b156ff77a1db06983cd
CharlieEL <charlieelemail@gmail.com>
2026-02-15 23:32:35 +0800

    Quartz sync: Feb 15, 2026, 11:32 PM

---
+++ content/Property Templete.md
@@ -0,0 +1,13 @@
+---
+title: Property Templete
+draft: true
+tags:
+---
+```md
+---
+title: Example Title
+draft: false
+tags:
+  - example-tag
+---
+```
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Charlie's Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -34,8 +34,8 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
+          secondary: "#7b2cbf",
+          tertiary: "#c77dff",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
@@ -45,8 +45,8 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
+          secondary: "#c77dff",
+          tertiary: "#7b2cbf",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },
